a62c4c095b422cfa2ea98caee1839cf1a5b2b36d
CuB1z <d.sanrin77@gmail.com>
2024-01-02 20:36:33 +0100

    Refactor Service class to calculate rent based on dice roll and simplify code

--- src/Service.java
+++ src/Service.java
@@ -26,54 +26,20 @@ public class Service extends Property{
 
     // Public methods =====================================================================================================
 
-    // Overriden doOperation() method
-    public void doOperation(Player p) {
-        Translator trs = this.terminal.getTranslatorManager().getTranslator();
-        String output;
-
-        // Make operations depending on the owner
-        if (this.getOwner() == null) super.doBuyOperation(p);
-
-        else if (this.getOwner() != p) {
-
-            if (this.isMortgaged()) {
-                output = trs.translate("You have landed on the property: %s, but it's mortgaged, you don't pay anything");
-                this.terminal.show(String.format(output, this.getDescription()));
-                return;
-            }
-
-            // Show property info
-            output = trs.translate("You have landed on the property: %s");
-            this.terminal.show(String.format(output, this.getDescription()));
-            this.terminal.show("");
-
-            // Get the dice number
-            this.terminal.show("Enter the number of the dice:");
-            int dice = this.terminal.readInt();
-            this.terminal.show("");
-
-            // Calculate the cost
-            int cost = this.getPaymentForRent() * dice;
-
-            // Show cost
-            output = trs.translate("You must pay %d");
-            this.terminal.show(String.format(output, cost));
-            this.terminal.show("");
-
-            // Pay mandatory cost
-            p.pay(cost, true);
-
-            // Make operations depending on the player's status
-            if (p.isBankrupt()) p.doBankruptcyTransference(this.getOwner());
-            else this.getOwner().receive(cost);
-
-        } else this.doOwnerOperation();
-    }
-
     // Method that returns the payment for rent
     public int getPaymentForRent() {
+        int dice = -1;
+
+        // Get the dice number
+        while (dice < 1) {
+            this.terminal.show("Enter the number of the dice:");
+            dice = this.terminal.readInt();
+            this.terminal.show("");
+        }
+
+        // Calculate the cost
         int count = this.getOwner().countServiceProperties();
-        return this.costStaying[count - 1];
+        return this.costStaying[count - 1] * dice;
     }
 
     // Getters & setters ==================================================================================================
