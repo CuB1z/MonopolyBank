1f1b2e53372b81cc49036b11caf1108a557cfac9
CuB1z <d.sanrin77@gmail.com>
2024-01-17 12:35:41 +0100

    Update language files and fix comment in Color.java

--- src/Color.java
+++ src/Color.java
@@ -2,7 +2,7 @@ package src;
 
 /* 
  * Enum used to represent the players in the game
- * Each player is assigned a color
+ * Each player is assigned to a color
  * If more colors are added, the game will be able to support more players
  * If wanted to change the game to be used in a different context, the colors can be changed by other names
 */
